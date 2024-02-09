90064749bed65017e6b5a116542e61f96af09f8b
z3y <z3y@protonmail.com>
2024-02-09 19:51:25 +0100

    fix 2d

--- Editor/Targets/Graph.hlsl
+++ Editor/Targets/Graph.hlsl
@@ -1,5 +1,10 @@
 #ifdef PREVIEW
-#define _WorldSpaceCameraPos float3(0, 0, -2.23)
-float4 _GraphTime;
-#define _Time _GraphTime
+    #ifdef PREVIEW3D
+        #define _WorldSpaceCameraPos float3(0, 0, -2.23)
+    #else
+        #define _WorldSpaceCameraPos float3(0, 0, -1)
+    #endif
+
+    float4 _GraphTime;
+    #define _Time _GraphTime
 #endif
--- Editor/Targets/Preview/Fragment.hlsl
+++ Editor/Targets/Preview/Fragment.hlsl
@@ -16,58 +16,81 @@ half4 frag(Varyings varyings) : SV_Target
 {
     // create data for preview
     float2 rawUV = UNPACK_UV0.xy;
-    // https://bgolus.medium.com/rendering-a-sphere-on-a-quad-13c92025570c
-    float2 uv = UNPACK_UV0.xy;
-    uv -= 0.5;
-    uv *= 1.02;
-    float3 spherePos = float3(0,0,0);
-    float3 rayDir = normalize(float3(uv, 1));
-    float3 rayOrigin = _WorldSpaceCameraPos.xyz;
-    
-    float rayHit = sphIntersect_Preview(rayOrigin, rayDir, float4(spherePos, 1));
-    float alpha3D = 1;
-    if (rayHit < 0) alpha3D = 0;
-
-    float3 positionWS = rayDir * rayHit + rayOrigin;
-    float3 normalWS = positionWS - spherePos;
-    float4 tangentWS = float4(cross(normalWS, float3(0.0, 1.0, 0.0)), -1.0);
-
-    tangentWS.xyz = tangentWS;
-
-    float dist = length(uv);
-    float pwidth = length(float2(ddx(dist), ddy(dist)));
-    alpha3D *= smoothstep(0.5, 0.5 - pwidth * 1.5, dist);
-
-    #ifdef UNPACK_POSITIONWS
-        UNPACK_POSITIONWS = positionWS;
-    #endif
-    #ifdef UNPACK_NORMALWS
-        UNPACK_NORMALWS = normalWS;
-    #endif
-    #ifdef UNPACK_TANGENTWS
-        UNPACK_TANGENTWS = tangentWS;
-    #endif
-
     #ifdef PREVIEW3D
-    normalWS = normalize(normalWS);
-    float2 generatedUV = float2(
-        atan2(normalWS.z, normalWS.x) / (UNITY_PI * 2.0),
-        acos(-normalWS.y) / UNITY_PI
-    );
-    generatedUV.x += 0.75;
+        // https://bgolus.medium.com/rendering-a-sphere-on-a-quad-13c92025570c
+        float2 uv = UNPACK_UV0.xy;
+        uv -= 0.5;
+        uv *= 1.02;
+        float3 spherePos = float3(0,0,0);
+        float3 rayDir = normalize(float3(uv, 1));
+        float3 rayOrigin = _WorldSpaceCameraPos.xyz;
+        
+        float rayHit = sphIntersect_Preview(rayOrigin, rayDir, float4(spherePos, 1));
+        float alpha3D = 1;
+        if (rayHit < 0) alpha3D = 0;
 
-    #ifdef UNPACK_UV0
-        UNPACK_UV0.xy = generatedUV;
-    #endif
-    #ifdef UNPACK_UV1
-        UNPACK_UV1.xy = generatedUV;
-    #endif
-    #ifdef UNPACK_UV2
-        UNPACK_UV2.xy = generatedUV;
-    #endif
-    #ifdef UNPACK_UV3
-        UNPACK_UV3.xy = generatedUV;
-    #endif
+        float3 positionWS = rayDir * rayHit + rayOrigin;
+        float3 normalWS = positionWS - spherePos;
+        float4 tangentWS = float4(cross(normalWS, float3(0.0, 1.0, 0.0)), -1.0);
+
+        tangentWS.xyz = tangentWS;
+
+        float dist = length(uv);
+        float pwidth = length(float2(ddx(dist), ddy(dist)));
+        alpha3D *= smoothstep(0.5, 0.5 - pwidth * 1.5, dist);
+
+        #ifdef UNPACK_POSITIONWS
+            UNPACK_POSITIONWS = positionWS;
+        #endif
+        #ifdef UNPACK_NORMALWS
+            UNPACK_NORMALWS = normalWS;
+        #endif
+        #ifdef UNPACK_TANGENTWS
+            UNPACK_TANGENTWS = tangentWS;
+        #endif
+
+        #ifdef PREVIEW3D
+        normalWS = normalize(normalWS);
+        float2 generatedUV = float2(
+            atan2(normalWS.z, normalWS.x) / (UNITY_PI * 2.0),
+            acos(-normalWS.y) / UNITY_PI
+        );
+        generatedUV.x += 0.75;
+
+        #ifdef UNPACK_UV0
+            UNPACK_UV0.xy = generatedUV;
+        #endif
+        #ifdef UNPACK_UV1
+            UNPACK_UV1.xy = generatedUV;
+        #endif
+        #ifdef UNPACK_UV2
+            UNPACK_UV2.xy = generatedUV;
+        #endif
+        #ifdef UNPACK_UV3
+            UNPACK_UV3.xy = generatedUV;
+        #endif
+        #endif
+
+    #else // 2d preview
+        #ifdef UNPACK_POSITIONWS
+            UNPACK_POSITIONWS = float3(UNPACK_UV0.xy - 0.5, 0) * 2.0;
+        #endif
+        float3 normalWS = float3(0,0,1);
+        #ifdef UNPACK_NORMALWS
+            UNPACK_NORMALWS = normalWS;
+        #endif
+        #ifdef UNPACK_TANGENTWS
+            UNPACK_TANGENTWS = float4(float3(1,0,0), 1.0);
+        #endif
+        #ifdef UNPACK_UV1
+            UNPACK_UV1.xy = UNPACK_UV0.xy;
+        #endif
+        #ifdef UNPACK_UV2
+            UNPACK_UV2.xy = UNPACK_UV0.xy;
+        #endif
+        #ifdef UNPACK_UV3
+            UNPACK_UV3.xy = UNPACK_UV0.xy;
+        #endif
     #endif
     
     #ifdef UNPACK_COLOR
