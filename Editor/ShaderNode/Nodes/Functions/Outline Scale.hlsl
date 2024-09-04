fcfd13f051551ed308c77ef0c217f83f7e0daabd
z3y <z3y@protonmail.com>
2024-09-05 00:08:18 +0200

    improve outline scale

--- Editor/ShaderNode/Nodes/Functions/Outline Scale.hlsl
+++ Editor/ShaderNode/Nodes/Functions/Outline Scale.hlsl
@@ -1,7 +1,17 @@
-void OutlineScale(float3 PositionOS, float3 NormalOS, out float3 PositionWS, float Width = 0.1)
+float3 headDirection(float3 positionWS)
+{
+#if defined(USING_STEREO_MATRICES)
+        return (unity_StereoWorldSpaceCameraPos[0].xyz + unity_StereoWorldSpaceCameraPos[1].xyz) * 0.5 - positionWS;
+#else
+    return _WorldSpaceCameraPos.xyz - positionWS;
+#endif
+}
+
+void OutlineScale(float3 PositionOS, float3 NormalOS, float3 PositionWS, out float3 ScaledPositionWS, float Width = 0.1, float WidthFix = 0.3)
 {
     #if defined(OUTLINE_PASS) || defined(UNITY_PASS_SHADOWCASTER)
-    PositionOS += NormalOS * Width;
+    Width *= lerp(1.0, saturate(length(headDirection(PositionWS))), WidthFix);
+    PositionOS += NormalOS * Width * 0.01;
     #endif
-    PositionWS = TransformObjectToWorld(PositionOS);
+    ScaledPositionWS = TransformObjectToWorld(PositionOS);
 }
