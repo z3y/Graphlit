01cc905426448bc5c585c0442fc1d84afaed6da9
z3y <z3y@protonmail.com>
2024-09-15 11:04:42 +0200

    added outline scale apply to shadow bool

--- Editor/ShaderNode/Nodes/Functions/Outline Scale.hlsl
+++ Editor/ShaderNode/Nodes/Functions/Outline Scale.hlsl
@@ -1,19 +1,26 @@
 #pragma once
 
-float3 headDirection(float3 positionWS)
+float3 OutlineHeadDirection(float3 positionWS)
 {
 #if defined(USING_STEREO_MATRICES)
-        return (unity_StereoWorldSpaceCameraPos[0].xyz + unity_StereoWorldSpaceCameraPos[1].xyz) * 0.5 - positionWS;
+    return (unity_StereoWorldSpaceCameraPos[0].xyz + unity_StereoWorldSpaceCameraPos[1].xyz) * 0.5 - positionWS;
 #else
     return _WorldSpaceCameraPos.xyz - positionWS;
 #endif
 }
 
-void OutlineScale(float3 PositionOS, float3 NormalOS, float3 PositionWS, out float3 ScaledPositionWS, float Width = 0.1, float WidthFix = 0.3)
+void OutlineScale(float3 PositionOS, float3 NormalOS, float3 PositionWS, out float3 ScaledPositionWS, float Width = 0.1, float WidthFix = 0.3, bool applyToShadow = true)
 {
     #if defined(OUTLINE_PASS) || defined(UNITY_PASS_SHADOWCASTER)
-    Width *= lerp(1.0, saturate(length(headDirection(PositionWS))), WidthFix);
-    PositionOS += NormalOS * Width * 0.01;
+
+        #ifdef UNITY_PASS_SHADOWCASTER
+            if (applyToShadow)
+        #endif
+        {
+            Width *= lerp(1.0, saturate(length(OutlineHeadDirection(PositionWS))), WidthFix);
+            PositionOS += NormalOS * Width * 0.01;
+        }
     #endif
+
     ScaledPositionWS = TransformObjectToWorld(PositionOS);
 }
