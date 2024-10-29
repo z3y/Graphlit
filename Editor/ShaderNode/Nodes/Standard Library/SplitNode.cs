5894343c297e807e124a67d174e33a42ea041c52
z3y <z3y@protonmail.com>
2024-10-29 15:43:16 +0100

    fix split

--- Editor/ShaderNode/Nodes/Standard Library/SplitNode.cs
+++ Editor/ShaderNode/Nodes/Standard Library/SplitNode.cs
@@ -13,35 +13,22 @@ namespace Graphlit
         const int A = 3;
         const int IN = 4;
 
-        Port _r;
-        Port _g;
-        Port _b;
-        Port _a;
-
         public override bool DisablePreview => true;
 
         public override void Initialize()
         {
             AddPort(new(PortDirection.Input, new Float(4, true), IN));
 
-            _r = AddPort(new(PortDirection.Output, new Float(1), R, "R"));
-            _g = AddPort(new(PortDirection.Output, new Float(1), G, "G"));
-            _b = AddPort(new(PortDirection.Output, new Float(1), B, "B"));
-            _a = AddPort(new(PortDirection.Output, new Float(1), A, "A"));
+            AddPort(new(PortDirection.Output, new Float(1), R, "R"));
+            AddPort(new(PortDirection.Output, new Float(1), G, "G"));
+            AddPort(new(PortDirection.Output, new Float(1), B, "B"));
+            AddPort(new(PortDirection.Output, new Float(1), A, "A"));
         }
 
         protected override void Generate(NodeVisitor visitor)
         {
             int c = GetDimensions(IN);
             string name = PortData[IN].Name;
-
-            _g.visible = c >= 2;
-            _b.visible = c >= 3;
-            _a.visible = c >= 4;
-            const int H = 24;
-            _g.style.height = c >= 2 ? H : 0;
-            _b.style.height = c >= 3 ? H : 0;
-            _a.style.height = c >= 4 ? H : 0;
 
             string zero = PrecisionString(1) + "(0)";
             SetVariable(R, c >= 1 ? name + ".x" : zero);
