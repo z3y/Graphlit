8a017043bb53e6f4706e04e5c42c42b031838256
z3y <z3y@protonmail.com>
2026-04-08 19:06:31 +0200

    disable optimizer

--- Runtime/Optimizer/GraphlitOptimizer.cs
+++ Runtime/Optimizer/GraphlitOptimizer.cs
@@ -1,21 +1,21 @@
-#if UNITY_EDITOR && NDMF_INCLUDED
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using VRC.SDKBase;
+// #if UNITY_EDITOR && NDMF_INCLUDED
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using VRC.SDKBase;
 
-namespace Graphlit.Optimizer
-{
-    public class GraphlitOptimizer : MonoBehaviour, IEditorOnly
-    {
-        public bool applyOnBuild = true;
-        [Tooltip("Maximum number of materials that can be merged into 1 Material.\nUnity has a maxium of 64 texture bindings allowed per shader. Reduce this value if the optimized shader doesn't render.")]
-        public int maxMaterialsPerBatch = 64;
-        public List<Material> excludedMaterials = new();
+// namespace Graphlit.Optimizer
+// {
+//     public class GraphlitOptimizer : MonoBehaviour, IEditorOnly
+//     {
+//         public bool applyOnBuild = true;
+//         [Tooltip("Maximum number of materials that can be merged into 1 Material.\nUnity has a maxium of 64 texture bindings allowed per shader. Reduce this value if the optimized shader doesn't render.")]
+//         public int maxMaterialsPerBatch = 64;
+//         public List<Material> excludedMaterials = new();
 
-        [Tooltip("VRChat fallback shaders can not work properly with the optimizer because multiple main textures get merged into one material. This texture can be used as a fallback instead (default is white).")]
-        public Texture2D fallbackMainTex = null;
-        public Vector4 fallbackMainTexScaleOffset = new(1, 1, 0, 0);
-    }
-}
-#endif
+//         [Tooltip("VRChat fallback shaders can not work properly with the optimizer because multiple main textures get merged into one material. This texture can be used as a fallback instead (default is white).")]
+//         public Texture2D fallbackMainTex = null;
+//         public Vector4 fallbackMainTexScaleOffset = new(1, 1, 0, 0);
+//     }
+// }
+// #endif
--- Runtime/Optimizer/GraphlitOptimizerEditor.cs
+++ Runtime/Optimizer/GraphlitOptimizerEditor.cs
@@ -1,26 +1,26 @@
-#if UNITY_EDITOR && NDMF_INCLUDED
-using System.Collections;
-using System.Collections.Generic;
-using nadena.dev.ndmf;
-using UnityEditor;
-using UnityEngine;
+// #if UNITY_EDITOR && NDMF_INCLUDED
+// using System.Collections;
+// using System.Collections.Generic;
+// using nadena.dev.ndmf;
+// using UnityEditor;
+// using UnityEngine;
 
-namespace Graphlit.Optimizer
-{
-    [CustomEditor(typeof(GraphlitOptimizer))]
-    public class GraphlitOptimizerEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
+// namespace Graphlit.Optimizer
+// {
+//     [CustomEditor(typeof(GraphlitOptimizer))]
+//     public class GraphlitOptimizerEditor : Editor
+//     {
+//         public override void OnInspectorGUI()
+//         {
+//             DrawDefaultInspector();
 
-            // GraphlitOptimizer optimizer = (GraphlitOptimizer)target;
+//             // GraphlitOptimizer optimizer = (GraphlitOptimizer)target;
 
-            // if (GUILayout.Button("Preview Optimizations"))
-            // {
+//             // if (GUILayout.Button("Preview Optimizations"))
+//             // {
 
-            // }
-        }
-    }
-}
-#endif
+//             // }
+//         }
+//     }
+// }
+// #endif
