438381b51071653af498e8f6c78bd4b746bdc465
z3y <z3y@protonmail.com>
2026-04-20 15:14:18 +0200

    apply only for avatars

--- Editor/Generation/TemplateOutput.cs
+++ Editor/Generation/TemplateOutput.cs
@@ -284,9 +284,12 @@ namespace Graphlit
         }
         protected static void AddFogKeywords(PassBuilder pass)
         {
-            // pass.pragmas.Add("#pragma multi_compile_fog");
+#if VRC_AVATARS_SDK_INCLUDED
             pass.pragmas.Add("#pragma multi_compile_vertex _ FOG_EXP2");
             pass.varyings.RequireCustom(1, "data.fogType", "FOG_TYPE");
+#else
+            pass.pragmas.Add("#pragma multi_compile_fragment _ FOG_LINEAR FOG_EXP FOG_EXP2");
+#endif
         }
         protected static void AddURPLightingPragmas(PassBuilder pass)
         {
